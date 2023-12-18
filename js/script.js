bec7aeab1356d3469d9c2a8aac93e99994c3edf3
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-18 17:33:58 +0100

    calculated ticket price for minors and over65

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 console.log('JS OK')
 
+
 //Chiediamo il numero di chilometri che l'utente vuole percorrere//
 const kilometersTravelled= parseInt(prompt('Quanti chilometri deve percorrere?', '20'));
-console.log(kilometersTravelled)
+console.log('Chilometri percorsi', kilometersTravelled)
 
 //Chiediamo l'età all'utente//
-const userAge= prompt("Qual'è l'età del passeggero?","35");
-console.log(userAge)
+const userAge= parseInt(prompt("Qual'è l'età del passeggero?","35"));
+console.log( 'Età utente', userAge)
 
 // Calcoliamo il prezzo del biglietto//
 const ticketPrice= 0.21 * kilometersTravelled;
-console.log(ticketPrice)
+console.log('Prezzo biglietto', ticketPrice)
+
+// Calcoliamo il prezzo del biglietto per i minorenni//
+const ticketPriceMinors = ticketPrice - (ticketPrice / 100 * 20);
+console.log('Prezzo biglietto minorenni', ticketPriceMinors.toFixed(2))
+
+// Calcoliamo il prezzo del biglietto per gli over 65//
+const ticketPriceOver65= ticketPrice - (ticketPrice / 100 * 40);
+console.log('Prezzo biglietto over65', ticketPriceOver65)
+
+// Calcoliamo il prezzo del biglietto per gli over 65//
