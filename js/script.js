802fe9ea99fd6ca781c9674f3b57d8b18c5b8a44
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-18 16:15:17 +0100

    calculated ticket price

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 console.log('JS OK')
 
 //Chiediamo il numero di chilometri che l'utente vuole percorrere//
-const kilometersTravelled= prompt('Quanti chilometri deve percorrere?', '20')
+const kilometersTravelled= parseInt(prompt('Quanti chilometri deve percorrere?', '20'));
 console.log(kilometersTravelled)
 
 //Chiediamo l'età all'utente//
-const userAge= prompt("Qual'è l'età del passeggero?","35")
+const userAge= prompt("Qual'è l'età del passeggero?","35");
 console.log(userAge)
 
 // Calcoliamo il prezzo del biglietto//
+const ticketPrice= 0.21 * kilometersTravelled;
+console.log(ticketPrice)
