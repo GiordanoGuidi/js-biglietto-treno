5c4a0778ae6f4399eac1138f4e7d7d5a99b04abb
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-18 15:59:23 +0100

    added prompt for kilometers and age

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+//Chiediamo il numero di chilometri che l'utente vuole percorrere//
+const kilometersTravelled= prompt('Quanti chilometri deve percorrere?', '20')
+console.log(kilometersTravelled)
+
+//Chiediamo l'età all'utente//
+const userAge= prompt("Qual'è l'età del passeggero?","35")
+console.log(userAge)
+
+// Calcoliamo il prezzo del biglietto//
